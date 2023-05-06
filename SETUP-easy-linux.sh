08ea7ebc81b5dfdf02f505565d21a45a20a42b78
Cory Beeson <37867832+Beesoc@users.noreply.github.com>
2023-05-06 15:27:04 -0500

    reworked vars

--- SETUP-easy-linux.sh
+++ SETUP-easy-linux.sh
@@ -5,7 +5,6 @@
 # Version: 0.0.2
 set -e
 
-scripts_dir=/opt/easy-linux
 compiled_dir=$HOME/compiled
 
 #sudo chmod a+x ${compiled_dir}/easy-linux/INSTALL.sh
@@ -37,41 +36,47 @@ Banner_func() {
 #  █ ▌▀ ▄ ╚ ╝ ╔ ╗ ═ ║  Characters used in the banner.
 }
 Banner_func
-printf "\\n${WT}                 Welcome to the Installer for Beesoc's Easy Linux.                   ${CY}${NC}\\n" 
-printf "\\n      ${CY}This installer will create the necessary folders and then clone the official\\n"
-printf "      repo for ${WT}Beesoc's Easy Linux ${CY}for installation. You will need a ${WT}Github username\\n"
+printf "\\n${WT}                 Welcome to the Installer for Beesoc\'s Easy Linux.         Press ${RED}[ctrl+c] ${CY}to cancel\\n"                 ${CY}${NC}\\n" 
+printf "\\n      ${CY}This installer will create the necessary folders and clone the official\\n"
+printf "      repo for ${WT}Beesoc\'s Easy Linux ${CY}for installation. You will need a ${WT}Github username\\n"
 printf "      username ${CY}and ${WT}fine-grained access token ${CY}to continue.\\n"
 printf "\\n  ${OG}If you need to create a fine grained personal access token, see here for instructions:${GN}\\n" 
-printf "https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token#creating-a-fine-grained-personal-access-token"
-printf " \\n"
-printf "${WT}\\n    For more info on Github's Personal Access Token see:\\n" 
-printf "${OG}https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token.\\n"
+printf "https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token#creating-a-fine-grained-personal-access-token\\n"
+printf "\\n"
+printf "${WT}\\n    For more info on Github\'s Personal Access Token see:\\n" 
+printf "${OG}https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token\\n"
 
 read -pr "Would you like to continue with the installation? [Y/n] " install
+install=${install:-Y}
+if [[ $install =~ ^[Yy]$ ]]; then
+  printf "Continuing${...}\\n"
+else
+  printf "Exiting.\\n"
+  exit 0
+fi
 
-
-printf " \\n${CY}Press ${WT}any ${CY}key to continue.                            Press ${RED}[ctrl+c] ${CY}to cancel\\n"
-#printf "${WT}\\n    ---->"
-printf "${WT} \\n"
-  read -r -n1 -s -t 60
+printf "${WT}\\n"
      if [[ ! -d ${compiled_dir} ]]; then
-       printf "  ${CY}${compiled_dir} directory not found.  Creating folder and ${WT}cloning Github${CY} repo.\\n"
+       printf "${CY}  ${compiled_dir} directory not found.  Creating folder and ${WT}cloning Github${CY} repo.\\n"
        mkdir ${compiled_dir}
      fi
-     if [[ ! -d ${compiled_dir}/easy-linux ]]; then
-           printf "  ${CY}${compiled_dir}/easy-linux directory not found.\\n"
-           printf "  ${CY}Creating folder and ${WT}cloning Github${CY} repo.\\n"
+     if [[ ! -d "${compiled_dir}/easy-linux" ]]; then
+           printf "${CY}  ${compiled_dir}/easy-linux directory not found.\\n"
+           printf "${CY}  Creating folder and ${WT}cloning Github ${CY}repo.\\n"
            mkdir ${compiled_dir}/easy-linux 
 #           git clone https://github.com/Beesoc/easy-linux.git
      fi
      if [[ -d "${compiled_dir}/easy-linux" ]]; then  
-       printf "  ${CY}Existing Github clone for Beesoc's Easy Linux found.\\n${CY}"
-       printf "       Please Wait, recloning Github repo to ${WT}${scripts_dir}.\\n"; sleep 1 
-       printf "${WT}.."
-       sudo rm -Rf ${compiled_dir}/easy-linux  
-       sleep 1; printf ".."; sleep 1
+       printf "  ${CY}Existing Github clone for Beesoc\'s Easy Linux found.\\n${CY}"
+       printf "       Please Wait, recloning Github repo.\\n" 
+       sleep 1 
+       printf "${WT}${...}"
+       sudo rm -Rf "${compiled_dir}/easy-linux"  
+       sleep 1
+       printf "${WT}${...}"
+       sleep 1
        cd ${compiled_dir} || exit
        git clone https://github.com/Beesoc/easy-linux.git
      fi  
     
-    source $HOME/compiled/easy-linux/INSTALL.sh
+    source "$HOME/compiled/easy-linux/INSTALL.sh"
